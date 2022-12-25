735cd7883e8b79fef5cee2432038ad03773b5070
devHudi <devhudi@gmail.com>
2022-12-25 22:41:07 +0900

    feat: HttpMessageNotReadableException 대응

--- src/main/kotlin/me/scribbble/backend/ui/ErrorHandler.kt
+++ src/main/kotlin/me/scribbble/backend/ui/ErrorHandler.kt
@@ -2,6 +2,7 @@ package me.scribbble.backend.ui
 
 import me.scribbble.backend.security.LoginFailedException
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -14,6 +15,12 @@ class ErrorHandler {
     fun handleBadRequest(e: Exception): ResponseEntity<ErrorResponse> {
         val response = ErrorResponse(e?.message ?: "")
         return ResponseEntity.badRequest().body(response)
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleHttpMessageNotReadable(e: Exception): ResponseEntity<ErrorResponse> {
+        val response = ErrorResponse("잘못된 HTTP Body 입니다.")
+        return ResponseEntity.ok(response)
     }
 
     @ExceptionHandler(LoginFailedException::class)
